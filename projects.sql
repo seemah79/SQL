fb79f0a5ba11c9fb5911c978921bc4812a158736
seemah79 <seemah.xworkz@gmail.com>
2024-06-26 19:43:03 +0530

    26/6 Database

---
+++ projects.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE projects;
+
+CREATE table airport_Details;
+CREATE table airport(id int, a_name varchar(10), a_arrival int, a_price bigint);
+
+CREATE table Cricket_teams;
+CREATE table teams(id int, t_name varchar(20), no_of_players int, t_caption varchar(6), t_coach varchar(20)); 
+
+
+
